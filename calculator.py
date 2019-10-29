5eab6d0f02bc33aa12cfd25990a469741964a341
celineg9 <57151451+celineg9@users.noreply.github.com>
2019-10-29 14:54:01 -0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,12 +1,18 @@
+import unittest
+import calculator
 
-def add(x, y):
-    return x + y 
+class Tests(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calculator.add(2,3),5) 
 
-def sub(x, y):
-    return x - y 
+    def test_sub(self):
+        self.assertEqual(calculator.sub(2,3),-1) 
 
-def mult(x, y):
-    return x + y 
+    def test_mult(self):
+        self.assertEqual(calculator.mult(2,3),6) 
 
-def div(x, y):
-    return x / y
+    def test_div(self):
+        self.assertEqual(calculator.div(6,3),2) 
+
+if __name__ == '__main__':
+    unittest.main()
